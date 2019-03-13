08bcfdc3900ccd4c5a75511d330e867dc11236c9
Hrombel <admin@hrombel.ru>
2019-03-13 17:36:23 +0300

    Added basic scan functionality

--- src/app/views/scan-view/scan-view.component.ts
+++ src/app/views/scan-view/scan-view.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { BarcodeFormat } from '@zxing/library';
+import { ZXingScannerComponent } from '@zxing/ngx-scanner';
 
 @Component({
   selector: 'app-scan-view',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScanViewComponent implements OnInit {
 
+  @ViewChild('scanner')
+  scanner: ZXingScannerComponent;
+
+  title = 'pricer-web';
+  scannerFormats = [ BarcodeFormat.QR_CODE ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  camerasFoundHandler(event: MediaDeviceInfo[]) {
+    let list = '';
+    event.forEach(it => {
+      list += it.label + '\n';
+    });
+    alert(list);
+    this.scanner.scan(event[0].deviceId);
+  }
+
+  camerasNotFoundHandler() {
+    alert('Устройств не найдено');
+  }
+
+  scanCompleteHandler(evt) {
+    alert(evt);
   }
 
 }
