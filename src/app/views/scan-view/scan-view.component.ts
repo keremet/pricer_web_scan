a0006b84ac413c868c10715fde0680440b2bf6ee
Hrombel <admin@hrombel.ru>
2019-03-13 22:42:05 +0300

    Basic scan view logic and fullscreen video

--- src/app/views/scan-view/scan-view.component.ts
+++ src/app/views/scan-view/scan-view.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Renderer2 } from '@angular/core';
 import { BarcodeFormat } from '@zxing/library';
 import { ZXingScannerComponent } from '@zxing/ngx-scanner';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-scan-view',
@@ -15,9 +17,20 @@ export class ScanViewComponent implements OnInit {
   title = 'pricer-web';
   scannerFormats = [ BarcodeFormat.QR_CODE ];
 
-  constructor() { }
+  constructor(private auth: AuthService,
+              private router: Router,
+              private renderer: Renderer2) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    if(!this.auth.loggedIn) {
+      this.router.navigateByUrl('/');
+      return;
+    }
+    
+    this.renderer.setStyle(this.scanner.previewElemRef.nativeElement, 'height', '100vh');
   }
 
   camerasFoundHandler(event: MediaDeviceInfo[]) {
