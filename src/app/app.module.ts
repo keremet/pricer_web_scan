0df8c7bce1ef7de3199639b2bc5d737d89d6c722
Hrombel <admin@hrombel.ru>
2019-03-13 20:56:51 +0300

    Added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { BasicViewComponent } from './views/basic-view/basic-view.component';
 import { ScanViewComponent } from './views/scan-view/scan-view.component';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginViewComponent } from './views/login-view/login-view.component';
+import { HeaderComponent } from './controls/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicViewComponent,
     ScanViewComponent,
-    LoginViewComponent
+    LoginViewComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
