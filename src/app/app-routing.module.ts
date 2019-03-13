e8f75e4df04b25a7b5ce2bfa563956655c1cdbc8
Hrombel <admin@hrombel.ru>
2019-03-13 22:43:11 +0300

    Set up application routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BasicViewComponent } from './views/basic-view/basic-view.component';
+import { ScanViewComponent } from './views/scan-view/scan-view.component';
+import { LoginViewComponent } from './views/login-view/login-view.component';
+import { IndexViewComponent } from './views/index-view/index-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BasicViewComponent,
+    children: [
+      {
+        path: '',
+        component: IndexViewComponent,
+      }
+    ]
+  },
+  {
+    path: 'scan',
+    component: ScanViewComponent,
+  },
+  {
+    path: 'login',
+    component: BasicViewComponent,
+    children: [
+      {
+        path: '',
+        component: LoginViewComponent,
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
