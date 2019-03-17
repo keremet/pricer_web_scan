30cc32daf51d4912dbc438d8b8063a2bcc5750dc
Hrombel <admin@hrombel.ru>
2019-03-17 23:21:17 +0300

    Added title and back button feature to header

--- src/app/controls/header/header.component.ts
+++ src/app/controls/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  @Input() back = false;
+  @Input() title = '';
+  @Output() backClick = new EventEmitter();
+
+  constructor() {
+  }
 
   ngOnInit() {
+
+  }
+
+  backClicked() {
+    this.backClick.emit();
   }
 
 }
