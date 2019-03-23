b61018e224558f0e6d38f5d81226cbcaa3e1d75f
Hrombel <admin@hrombel.ru>
2019-03-23 22:00:39 +0300

    Added receipts view

--- src/app/views/receipts-view/receipts-view.component.ts
+++ src/app/views/receipts-view/receipts-view.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ReceiptScan } from 'src/app/data/receipts/receipt';
+import { ReceiptsService } from 'src/app/services/receipts/receipts.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-receipts-view',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReceiptsViewComponent implements OnInit {
 
-  constructor() { }
+  private _scanned: ReceiptScan[];
+  private _sent: ReceiptScan[];
+
+  constructor(private api: ReceiptsService,
+              private auth: AuthService,
+              private router: Router) {}
 
   ngOnInit() {
+    this._scanned = this.api.scanned;
+    this._sent = this.api.sent;
+  }
+
+  ngAfterViewInit() {
+    if(!this.auth.loggedIn) {
+      this.router.navigateByUrl('/');
+      return;
+    }
+  }
+
+  get scanned() {
+    return this._scanned;
+  }
+
+  get sent() {
+    return this._sent;
   }
 
 }
